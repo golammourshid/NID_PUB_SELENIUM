359430c09af81ef6c49e083f708821fa2ec5fa28
golammourshid <golammourshid100@gmail.com>
2023-09-26 20:30:47 +0600

    Some modification in get_started.py file and ommit to use the data from csv

--- Get_Started.py
+++ Get_Started.py
@@ -13,29 +13,33 @@ driver = webdriver.Chrome(service=ser, options=op)
 
 
 driver.get("https://prportal.nidw.gov.bd/nid-pub/")
+driver.maximize_window()
 driver.implicitly_wait(25)
 # driver.maximize_window()
 
-file = open('get_started.csv', encoding='utf-8')
-csvreader = csv.reader(file)
+# file = open('get_started.csv', encoding='utf-8')
+# csvreader = csv.reader(file)
+
 # header = []
 # header = next(csvreader)
-next(csvreader)
-rows = []
-for row in csvreader:
-    rows.append(row)
-print(rows[0][0])
+# next(csvreader)
+# rows = []
+# for row in csvreader:
+#     rows.append(row)
+# print(rows[0][0])
 
 time.sleep(2)
 
-# Right menu
-driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div[3]/div[2]/i").click()
+# Right menu for small window
+# driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/div[3]/div[2]/i").click()
 # time.sleep(2)
 
 driver.implicitly_wait(5)
 
 # English Language
-driver.find_element(By.XPATH, "/html/body/div[1]/div[4]/div[2]/div[1]/span").click()
+driver.find_element(By.ID, 'language').click()
+driver.find_element(By.XPATH, '//*[@id="language-option"]/div[1]').click()
+# driver.find_element(By.XPATH, "/html/body/div[1]/div[4]/div[2]/div[1]/span").click()
 # time.sleep(2)
 
 driver.implicitly_wait(5)
@@ -47,20 +51,20 @@ driver.find_element(By.LINK_TEXT, 'Get Started').click()
 driver.implicitly_wait(5)
 
 # Provide full name
-driver.find_element(By.NAME, 'fullName').send_keys(rows[0][0])
+driver.find_element(By.NAME, 'fullName').send_keys('Golam Mourshid Antorr')
 
 # provide date of birth -> day
-driver.find_element(By.NAME, 'day').send_keys(rows[0][1])
+driver.find_element(By.NAME, 'day').send_keys('1')
 
 # provide date of birth -> month
-driver.find_element(By.NAME, 'month').send_keys(rows[0][2])
+driver.find_element(By.NAME, 'month').send_keys('1')
 
 # provide date of birth -> year
-driver.find_element(By.NAME, 'year').send_keys(rows[0][3])
+driver.find_element(By.NAME, 'year').send_keys('2000')
 
 
 # Provide Captcha
-driver.find_element(By.NAME, 'captcha').send_keys(rows[0][4])
+driver.find_element(By.NAME, 'captcha').send_keys('123456')
 
 time.sleep(2)
 
@@ -72,10 +76,10 @@ driver.find_element(By.ID, 'start').click()
 time.sleep(2)
 
 # Select Country Code
-Select(driver.find_element(By.ID, 'country-select')).select_by_visible_text(rows[0][9])
+Select(driver.find_element(By.ID, 'country-select')).select_by_visible_text('Italy (IT)+39')
 
 # Give Mobile Number
-driver.find_element(By.NAME, 'mobile').send_keys(rows[0][5])
+driver.find_element(By.NAME, 'mobile').send_keys('3279739401')
 
 time.sleep(2)
 driver.implicitly_wait(5)
@@ -86,8 +90,8 @@ driver.find_element(By.ID, 'send-sms').click()
 driver.implicitly_wait(15)
 time.sleep(2)
 # Give Verification Code
-driver.find_element(By.NAME, 'verificationCode').send_keys(rows[0][4])
-
+driver.find_element(By.NAME, 'verificationCode').send_keys('123456')
+driver.implicitly_wait(15)
 time.sleep(2)
 
 # Continue Button
@@ -95,13 +99,13 @@ driver.find_element(By.ID, 'verify-mobile-code').click()
 
 time.sleep(2)
 # Give Username
-driver.find_element(By.NAME, 'username').send_keys(rows[0][6])
+driver.find_element(By.NAME, 'username').send_keys('31antor')
 
 # Give Password
-driver.find_element(By.NAME, 'password').send_keys(rows[0][7])
+driver.find_element(By.NAME, 'password').send_keys('123456')
 
 # Give Retype Password
-driver.find_element(By.NAME, 'retypePassword').send_keys(rows[0][7])
+driver.find_element(By.NAME, 'retypePassword').send_keys('123456')
 
 time.sleep(2)
 
@@ -109,7 +113,7 @@ time.sleep(2)
 driver.find_element(By.ID, 'create-account').click()
 
 time.sleep(10)
-print("\n\n\nAccount Created!")
+print("Account Created Successfully!\n")
 
 
 
